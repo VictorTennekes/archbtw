2bdfeed475acd26e9017399d2545b1994cddc806
Victor Tennekes <victortennekes@icloud.com>
2025-11-10 15:58:29 +0100

    feat: add idle and fingerprint

--- config/mango/autostart.sh
+++ config/mango/autostart.sh
@@ -1,4 +1,6 @@
-pamixer --set-volume 40 --source alsa_input.pci-0000_c1_00.6.analog-stereo
+hypridle &
+pamixer --set-volume 40 --source alsa_input.pci-0000_c1_00.6.analog-stereo &
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark' &
 swaybg -i ~/Images/wallpapers/000076340017.jpg &
 waybar -c ~/.config/mango/waybar/config.jsonc -s ~/.config/mango/waybar/style.css &
+systemctl --user start hyprpolkitagent &
