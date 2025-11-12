c82f4b0c983242041b9650da8824559edd941c88
Victor Tennekes <victortennekes@icloud.com>
2025-11-12 12:41:42 +0100

    feat: update mango scroller look and screenshot

--- config/mango/scripts/screenshot.sh
+++ config/mango/scripts/screenshot.sh
@@ -5,12 +5,17 @@ mkdir -p "$save_dir"
 
 timestamp=$(date +'%a_%T_')
 
+filename="$save_dir/screen_${timestamp}.png"
+
+
 if [[ "$1" == "--save" ]]; then
-    grim -g "$(slurp)" "$save_dir/screen_${timestamp}.png"
+    grim -g "$(slurp)" $filename
     echo "Screenshot saved to $save_dir/screen_${timestamp}.png"
 elif [[ "$1" == "--copy" ]]; then
     grim -g "$(slurp)" - | wl-copy
     echo "Screenshot copied to clipboard"
+elif [[ "$1" == "--full" ]]; then
+	grim $filename 
 else
     echo "Usage: $0 --save | --copy"
     exit 1
