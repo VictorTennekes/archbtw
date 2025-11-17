add824929dc13e5757e7401700fc86c25add6721
Victor Tennekes <victortennekes@icloud.com>
2025-11-17 22:59:58 +0100

    feat: set ghostty default term

--- config/zsh/config/exports.zsh
+++ config/zsh/config/exports.zsh
@@ -1,6 +1,7 @@
 # --- App Configurations ---
 export FZF_DEFAULT_COMMAND='fd --type f'
 export STARSHIP_CONFIG="$HOME/.config/zsh/starship.toml"
+export TERMINAL=ghostty
 
 # --- Terminal Colors ---
 export CLICOLOR=1
